7c53862eaf2cefb6c9ac84108e24de3780b7f67e
7FutEki <105148135+7FutEki@users.noreply.github.com>
2023-03-15 21:44:42 +0400

    Add files via upload

---
+++ Datagrid/DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Spisok> spisok = new ObservableCollection<Spisok>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt_grid.ItemsSource= spisok;
+        }
+        public class Spisok
+        {
+            public string Thing { get; set; }
+            public string Date { get; set; }
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            SpisokAdd spisokAdd = new SpisokAdd();
+            if (spisokAdd.ShowDialog() == true)
+            {
+                spisok.Add(new Spisok { Thing = spisokAdd.Thing, Date = spisokAdd.Date});
+            }
+
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                spisok.Remove((Spisok)dt_grid.SelectedItem);
+            }
+            catch 
+            {
+                MessageBox.Show("Брат, оно того не стоит");
+            }
+            
+        }
+    }
+}
---
+++ Datagrid/DataGrid/SpisokAdd.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// Логика взаимодействия для SpisokAdd.xaml
+    /// </summary>
+    public partial class SpisokAdd : Window
+    {
+        public string Thing { get; set; }
+        public string Date { get; set; }
+        public SpisokAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            Thing = tb_thing.Text;
+            Date = tb_date.Text;
+            DialogResult = true;
+        }
+    }
+}
